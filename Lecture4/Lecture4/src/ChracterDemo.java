c92d913ab463c29c8f72e1f21cf70f0c38481fb1
Shiham Samsudeen <shihams247@gmail.com>
2022-09-18 10:34:27 +0530

    Characters demo

---
+++ Lecture4/Lecture4/src/ChracterDemo.java
@@ -0,0 +1,11 @@
+public class ChracterDemo {
+    public static void main(String[] args)
+    {
+        int age = 30;
+
+        if( age >= 30 ){
+            System.out.println(" You are matured.");
+        }
+
+    }
+}
